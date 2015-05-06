1befd33ac85d4a85b8c9117624d20ecdba4a281b
Katharina Zwinger <katharina@more-onion.com>
2015-05-06 18:06:50 +0200

    Create gh-pages branch

---
+++ javascripts/theme.js
@@ -0,0 +1,69 @@
+$(window).load(function(){
+  // two column layout
+  sortTwoColumn();
+  $('.eaSubmitResetButtonGroup').appendTo($('.en_right_wrapper').last());
+  $('body').addClass('twocolumn');
+
+  // put right column before left column
+  $('.en_left_wrapper').each(function(){
+    var id = $(this).attr('id').match(/[0-9]+$/);
+    $(this).before($('#right_wrapper' + id[0]));
+  });
+
+  // enable Picker and Selector
+  // see http://www.benplum.com/formstone/
+  if (typeof $.fn.selecter == 'function') {
+    $('select').selecter();
+  }
+  if (typeof $.fn.picker == 'function') {
+    $('input[type=radio], input[type=checkbox]').picker();
+  }
+
+  // consolidate markup:
+  // * add missing div around textareas to apply wobbles
+  // * move all mandatory field marker inside the label
+  $('textarea').each(function() {
+    var $parents = $('.eaFormField, .eaSwitchSubjectContainer, .eaMessageContentContainer, .eaQuestionTextareaFormFieldContainer, .textarea-wrapper');
+    if ($(this).parent().not($parents)) {
+      $(this).wrap("<div class='textarea-wrapper'></div>");
+    }
+  });
+  $('label + .eaMandatoryFieldMarker').each(function() {
+    $(this).prev('label').append($(this));
+  });
+
+  // move validation icon next to label
+  // and the error message below the label
+  $('.eaErrorMessage').each(function() {
+    var self = $(this);
+    var label = self.siblings('.eaFormElementLabel');
+    var field = self.siblings('.eaFormField');
+    var icon = $('.eaValidationIcon', label.parent());
+    icon.appendTo(label);
+    self.appendTo(field);
+  });
+
+  // add class to field where error occured
+  $(window).on('DOMSubtreeModified', '.eaErrorMessage', function(e) {
+    var self = $(e.target);
+    if (!self.is(':empty')) {
+      self.closest('.eaFormField').addClass('validationError');
+    }
+  });
+
+  // add "read more" link functionality
+  $('.background-info-hidden').hide();
+  $('.info-toggle').css('display', 'block');
+  $('.info-toggle').on('click', function(e) {
+    var $toggle = $(this);
+    var $target = $('#background-info');
+    if ($target.length > 0) {
+      $target.slideDown('fast', function(){
+        $toggle.hide();
+      });
+    }
+    e.preventDefault();
+    return false;
+  });
+
+});
